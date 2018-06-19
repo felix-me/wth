fd718bc2c291abedf42298436229ee8ec1745069
FelixM <felixm.manchester@gmail.com>
2018-06-19 10:50:49 +0100

    Implemented game loop

--- src/com/JHEF/wth/window/Game.java
+++ src/com/JHEF/wth/window/Game.java
@@ -1,6 +1,7 @@
 package com.JHEF.wth.window;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 /**
  * @author WTH
@@ -36,7 +37,71 @@ public class Game extends Canvas implements Runnable {
      */
     public void run()
     {
-        System.out.println("Thread has begun");
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0; // FPS
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while (running)
+        {
+            long now = System.nanoTime();
+            delta += (now - lastTime) /ns;
+            lastTime = now;
+            while (delta >= 1)
+            {
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000)
+            {
+                timer += 1000;
+                System.out.println("FPS: " + frames + " TICKS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+    }
+
+    /**
+     * Updates to the system
+     */
+    public void tick()
+    {
+
+    }
+
+    /**
+     * Graphical components
+     * Images
+     * Backgrounds
+     * Sprites
+     * Limit FPS here through {@link BufferStrategy}
+     */
+    public void render()
+    {
+        BufferStrategy bs = this.getBufferStrategy(); // From Canvas
+        // Make sure that buffer is not always set to null when render called
+        if(bs == null)
+        {
+            this.createBufferStrategy(3); // load in 3 buffers if possible
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        // Draw game here
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, getWidth(), getHeight()); // Stop flickering
+
+
+        g.dispose();
+        bs.show();
     }
 
     /**
