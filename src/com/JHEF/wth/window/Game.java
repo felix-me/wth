dd576875e08196448091abfaca97922cb457427d
FelixM <felixm.manchester@gmail.com>
2018-06-20 13:56:36 +0100

    solved merge conflicts

--- src/com/JHEF/wth/window/Game.java
+++ src/com/JHEF/wth/window/Game.java
@@ -307,6 +307,15 @@ public class Game extends Canvas implements Runnable {
         return gameInstance;
     }
 
+    private static BufferedImage resize(BufferedImage img, int height, int width) {
+        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return resized;
+    }
+
     /**
      * Main method for running WTH.
      * @param args standard main method param
