327d75a051830816a0ae0ef1e35a99ccf9cb8f52
FelixM <felixm.manchester@gmail.com>
2018-06-21 12:13:16 +0100

    Added relative filepaths to sound

--- src/com/JHEF/wth/framework/Sound.java
+++ src/com/JHEF/wth/framework/Sound.java
@@ -9,10 +9,12 @@ public class Sound {
     File f;
     AudioInputStream ais;
     Clip clip;
+    String currentDir;
 
         public void playSound(String file) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
-            f = new File(file);
+            currentDir = System.getProperty("user.dir")+"/res";
+            f = new File(currentDir+file);
             ais = AudioSystem.getAudioInputStream(f);
             clip = AudioSystem.getClip();
             clip.open(ais);
